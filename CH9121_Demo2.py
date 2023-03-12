d10932718d0198957bb7e71205f09a0a111555cf
CEMSguy <121373907+Danielerikrust@users.noreply.github.com>
2023-03-11 23:49:24 -0800

    Add files via upload

---
+++ CH9121_Demo2.py
@@ -0,0 +1,11 @@
+"""Demo file for using CH9121.py with a Waveshare uart to ethernet device"""
+from CH9121 import CH9121
+
+
+"""CH9121 Ethernet Setup Section using the default values in CH9121.py
+Change the default network variables in CH9121.py if your settings are unchanging"""
+#eth = CH9121(uarts=1,config=True)      # Initialize the CH9121 device for Uart0 only, auto config()
+eth = CH9121(uarts=2,config=True)       # Initialize the CH9121 device for both Uart0 & Uart1, auto config()
+
+eth.readsettings()
+
